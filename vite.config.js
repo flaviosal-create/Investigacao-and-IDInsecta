c54fd3e3d566d7b552fe71c534b41af8fd1af312
Flavio Dias Vieira <flaviodiasvieira@iMac-de-Flavio.local>
2026-08-24 14:50:33 -0300

    fix: corrige hmr da visualizacao local

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const disableDevClient = {
-  name: "disable-vite-client-without-hmr",
-  apply: "serve",
-  transformIndexHtml: {
-    order: "post",
-    handler(html) {
-      return html.replace(
-        /\s*<script type="module" src="\/\@vite\/client"><\/script>/,
-        ""
-      );
-    },
-  },
-};
-
 export default defineConfig({
-  plugins: [react(), disableDevClient],
+  plugins: [react()],
   server: {
     strictPort: true,
-    hmr: false,
   },
 });

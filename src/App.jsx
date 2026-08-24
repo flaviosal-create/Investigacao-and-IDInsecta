698d0f18a4b05a82a8a65bf3ff37e82ee1360afb
Flavio Dias Vieira <flaviodiasvieira@iMac-de-Flavio.local>
2026-08-24 12:03:35 -0300

    fix: inicia diretamente na chave de Insecta

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
   const [
     activePage,
     setActivePage,
-  ] = useState("universo");
+  ] = useState("insecta-key");
   const [
     activeInvestigationPanel,
     setActiveInvestigationPanel,
